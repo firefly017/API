9dc7afaba929f6a45399c4d85c9fe191c564d68b
firefly017 <jui.n.nahar@gmail.com>
2024-08-01 18:35:13 +0200

    updated readme file

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # API
+
+1. fancy car gallery : https://fancy-car-gallery.netlify.app/
+2. list of names: https://list-of-names.netlify.app/
--- unsplash_api_gallery/index.js
+++ unsplash_api_gallery/index.js
@@ -1,5 +1,5 @@
 const divForImg = document.getElementById("img-box");
-//enter your api key where it says YOUR_ACCESS_KEY
+
 const requestOne =
   "https://api.unsplash.com/search/photos?page=1&query=&client_id=eehCxl9WM_E9WTVQggtbpfA5y_nz6zQ5Hlmavj3LIU4";
 const requestTwo =
@@ -8,18 +8,14 @@ function makeRequestToUnsplash(requestUrl) {
   fetch(requestUrl)
     .then((res) => res.json())
     .then((data) => {
-      //we are actually using the returned data from the API here
-      // data.results contains an array of objects so we can use an array method here to iterate
       data.results.forEach((imageObj) => {
         createImage(imageObj);
       });
     });
 }
-// the function createImage() below is a helper function used to generate an image tag for use in our web page
 function createImage(imageObj) {
   const imageDiv = document.createElement("div");
   const image = document.createElement("img");
-  // styling for the elements
   image.src = imageObj.urls.regular;
   image.alt = imageObj.alt_description;
   image.style.margin = "20px";
@@ -29,7 +25,6 @@ function createImage(imageObj) {
   imageDiv.append(image);
   divForImg.append(imageDiv);
 }
-//each call to makeRequestToUnsplash() returns data with 10 images in it
-//so I make two calls to it to get 20 images to populate the page with images
+
 makeRequestToUnsplash(requestOne);
 makeRequestToUnsplash(requestTwo);
